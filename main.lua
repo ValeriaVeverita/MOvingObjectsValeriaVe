a47f18127b2f23786b22e10ae7514741a0a9f880
Valeria Veverita <valeria.veverita@ocsbstudent.ca>
2018-03-01 19:56:02 -0500

    NewChanges

--- main.lua
+++ main.lua
@@ -34,3 +34,31 @@ end
 
 -- MoveShip will be called over and over again
 Runtime:addEventListener("enterFrame", MoveShip)
+
+-- character image with width and height
+local octopus = display.newImageRect("Images/octopus.png", 200, 200)
+
+
+
+-- set the initial x and y position of the octopus
+octopus.x = 0
+octopus.y = 0
+
+--Funnction: MoveOctopus
+--Input
+--Output
+--Description: This function adda the scrool
+--Condition: AppearOctopus
+--Description: when beetleship dissapear, then make the octopus appear
+if beetleship.x == 90 then
+ local function MoveOctopus( event)
+	  -- add the scroll speed to the x-value and y-value of the octopus
+     octopus.x = octopus.x + scrollSpeed
+      octopus.y = octopus.y + scrollSpeed
+      -- add opacity to the object
+      octopus.alpha = octopus.alpha + 0.01
+ end
+   --MoveOctopus will be called over and over again
+  Runtime:addEventListener("enterFrame", MoveOctopus)
+end
+
