810a2ae5d0e19aed8bf53248a242ec3ac81231dc
Valeria Veverita <valeria.veverita@ocsbstudent.ca>
2018-03-05 11:06:01 -0500

    added

--- main.lua
+++ main.lua
@@ -10,10 +10,11 @@ display.setStatusBar ( display.HiddenStatusBar)
 scrollSpeed = 3
 
 -- background image with width and height
-local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+local backgroundImage = display.newImageRect("Images/space.png", 2048, 1536)
 
 --caharcter image with width and height
 local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+
 --set the image to be transparent
 beetleship.alpha = 0
 
@@ -26,11 +27,46 @@ beetleship.y = display.contentHeight/3
 --Output: none
 -- Description: Thsi function adds the scrool speed to the x-value of the ship
 local function MoveShip( event )
+  -- print to the console the position of the beetleship
+  print ("***beetleship.x = " .. beetleship.x)
+
   -- add the scroll speed to the x-value of the ship
   beetleship.x = beetleship.x + scrollSpeed
   -- change the transperancy of the ship every time it moves so that it fades out
-   beetleship.alpha = beetleship.alpha + 0.01
+  beetleship.alpha = beetleship.alpha + 0.01
+  
 end
 
 -- MoveShip will be called over and over again
 Runtime:addEventListener("enterFrame", MoveShip)
+
+
+local octopus = display.newImageRect("Images/octopus.png", 200, 200)
+
+local scrollSpeed2 = 4
+
+-- set the initial x and y position of the octopus
+octopus.x = 0
+octopus.y = 0
+octopus.alpha = 0
+
+--Funnction: MoveOctopus
+--Input
+--Output
+--Description: This function adda the scrool
+--Condition: AppearOctopus
+--Description: when beetleship dissapear, then make the octopus appear
+local function MoveOctopus( event )
+
+  if (beetleship.x >= 1200) then
+    print ("***octopus.x" .. octopus.x)
+    octopus.x = octopus.x + scrollSpeed2
+    octopus.y = octopus.y + scrollSpeed2
+    octopus.alpha  = octopus.alpha + 0.02
+  end
+end 
+
+-- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", MoveOctopus)
+
+-- create a new object 
