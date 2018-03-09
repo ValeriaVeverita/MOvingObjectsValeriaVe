453388d0e67fd1c71ea04db7af9a83288f1bc4d3
Valeria Veverita <valeria.veverita@ocsbstudent.ca>
2018-03-09 10:41:20 -0500

    Change

--- main.lua
+++ main.lua
@@ -34,64 +34,77 @@ local function MoveShip( event )
   beetleship.x = beetleship.x + scrollSpeed
   -- change the transperancy of the ship every time it moves so that it fades out
   beetleship.alpha = beetleship.alpha + 0.01
-  
+  --make the object to spin
+  beetleship:rotate(50) 
 end
 
 -- MoveShip will be called over and over again
 Runtime:addEventListener("enterFrame", MoveShip)
 
-<<<<<<< HEAD
-
+--create another character and add the scroll spedd
 local octopus = display.newImageRect("Images/octopus.png", 200, 200)
-
 local scrollSpeed2 = 4
-=======
--- character image with width and height
-local octopus = display.newImageRect("Images/octopus.png", 200, 200)
-
-
->>>>>>> a47f18127b2f23786b22e10ae7514741a0a9f880
-
 -- set the initial x and y position of the octopus
 octopus.x = 0
 octopus.y = 0
-<<<<<<< HEAD
+--set the transperancy of the object
 octopus.alpha = 0
-=======
->>>>>>> a47f18127b2f23786b22e10ae7514741a0a9f880
+--set the scale of the object
+octopus:scale(1,1)
 
 --Funnction: MoveOctopus
---Input
---Output
+--Input: this function accepts and event listener
+--Output: none
 --Description: This function adda the scrool
 --Condition: AppearOctopus
 --Description: when beetleship dissapear, then make the octopus appear
-<<<<<<< HEAD
+
 local function MoveOctopus( event )
 
   if (beetleship.x >= 1200) then
     print ("***octopus.x" .. octopus.x)
+    --make the vharacter to move diagonoly by adding the scroll Speed to the x-value and y-value
     octopus.x = octopus.x + scrollSpeed2
     octopus.y = octopus.y + scrollSpeed2
+    --cahnge the transperency of the ship every time it moves
     octopus.alpha  = octopus.alpha + 0.02
+    --make the object grow by adding value to the xScale and yScale
+    octopus.xScale = octopus.xScale + 0.01
+    octopus.yScale = octopus.yScale + 0.01
   end
 end 
 
 -- MoveShip will be called over and over again
 Runtime:addEventListener("enterFrame", MoveOctopus)
 
--- create a new object 
-=======
-if beetleship.x == 90 then
- local function MoveOctopus( event)
-	  -- add the scroll speed to the x-value and y-value of the octopus
-     octopus.x = octopus.x + scrollSpeed
-      octopus.y = octopus.y + scrollSpeed
-      -- add opacity to the object
-      octopus.alpha = octopus.alpha + 0.01
- end
-   --MoveOctopus will be called over and over again
-  Runtime:addEventListener("enterFrame", MoveOctopus)
-end
 
->>>>>>> a47f18127b2f23786b22e10ae7514741a0a9f880
+-- create a new object 
+local rocket = display.newImageRect("Images/rocketship.png", 300, 300)
+--Make it transparent
+rocket.alpha = 1
+--set the coordinates of the object
+rocket.x = -150
+rocket.y = 350
+
+--Set the local variable of tyhe scroll speed
+local scrollSpeed3 = 3
+
+--Function: MoveRocket
+--Condition: Appear rocket
+--Input: this function accepts and event listener
+--Output: none
+--Description: when octopus dissapear, then rocket will move in one direction a nd when it reaches one point it will move in opposite direction
+local function MoveRocket( event )
+  if (octopus.x >= 500) then
+   rocket.x = rocket.x +scrollSpeed3
+  end
+ if (rocket.x<0) then
+    scrollSpeed3 = 3
+    rocket.xScale= 1
+ end
+ if (rocket.x>1000) then
+    scrollSpeed3 = -3
+    rocket.xScale= -1
+  end
+end
+Runtime:addEventListener("enterFrame",MoveRocket)
